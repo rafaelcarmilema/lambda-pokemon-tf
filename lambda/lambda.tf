467dd5442c6ab24b6674b29763aca6cff536ea74
Rafael Carmilema <rafaelcarmilema@gmail.com>
2022-08-16 19:55:32 -0400

    added bundle

--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -1,9 +1,9 @@
 resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a 
   # path.module in the filename.
-  filename      = "lambda_function_payload.zip"
+  filename      = "../appcode/lambda_function_payload.zip"
   function_name = "lambda_function_pokemon"
-  role          = data.terraform_remote_state.iam.lambda_role
+  role          = module.lib.lambda_role
   handler       = "index.test"
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
--- lib/main.tf
+++ lib/main.tf
@@ -3,11 +3,14 @@ data "terraform_remote_state" "iam_tf" {
 
   config = {
     bucket = "lambda-pokemon-tf"
-    key    = "iam"
+    key    = "iam/terraform.tfstate"
     region = "us-east-1"
   }
 }
 
+output "lambda_role" {
+  value = data.terraform_remote_state.iam_tf.outputs.lambda_role
+}
 # data "aws_s3_bucket" "iam_role" {
 #   bucket = "lambda-pokemon-tf"
 # }
