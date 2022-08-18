d515b94f1d2587f9ce14750ad388bf2f0542d260
Rafael Carmilema <rafaelcarmilema@gmail.com>
2022-08-18 17:30:18 -0400

    fixed

--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -1,15 +1,15 @@
 resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a 
   # path.module in the filename.
-  filename      = "../appcode/my_bundle.zip"
+  filename      = "../appcode/bundle/my_bundle.zip"
   function_name = "lambda_function_pokemon"
   role          = module.lib.lambda_role
-  handler       = "index.test"
-
+  handler       = "main.py"
+  description = "GET request to pokemon api and send an email via SNS to sub users with random pokemon facts"
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256("../appcode/my_bundle.zip")
+  source_code_hash = filebase64sha256("../appcode/bundle/my_bundle.zip")
 
   runtime = "python3.8"
 
