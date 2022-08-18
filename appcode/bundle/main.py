e2faab1d82384b66bb29f655e875a80a07ed0fc2
Rafael Carmilema <rafaelcarmilema@gmail.com>
2022-08-18 17:22:42 -0400

    fixed

--- appcode/bundle/main.py
+++ appcode/bundle/main.py
@@ -23,7 +23,8 @@ def iterate_json(data):
 
 def lambda_handler(event, context):
     data = call_api()
-    iterate_json(data)
+    return  iterate_json(data)
+
     # Get the object from the event and show its content type
     # bucket = event['Records'][0]['s3']['bucket']['name']
     # key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
