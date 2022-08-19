0d9a76a544a4f73f7ae088f6a39eee21c7793b12
Rafael Carmilema <rafaelcarmilema@gmail.com>
2022-08-19 11:54:54 -0400

    added pub to sns

--- appcode/bundle/main.py
+++ appcode/bundle/main.py
@@ -21,12 +21,13 @@ def call_api_random_pokefact(random_pokemon):
     api_url = "https://pokeapi.co/api/v2/pokemon/"+random_pokemon
     response = requests.get(api_url)
     json_file = response.json()
+    
     id = print(json_file["id"])
-    id = print(json_file["name"])
-    id = print(json_file["height"])
-    id = print(json_file["weight"])
+    name = print(json_file["name"])
+    height = print(json_file["height"])
+    weight = print(json_file["weight"])
     #data = json_file['results']
-    return id
+    return [id,name,height,weight]
 
 
 # iterate thru the json file get only name and url for each pokemon
@@ -35,16 +36,40 @@ def iterate_json(data):
         print(pokemon['name']+" "+pokemon['url']) 
 
 def lambda_handler(event, context):
-    client = boto3.client('ssm')
-    data = call_api()
+    client = boto3.client('sns')
+
+    random_pokemon = str(randrange(1,95)) # generate random pokemon 
+    print(random_pokemon)
+    ##Call api and get pokemon info from a random pokemon
+    data = call_api_random_pokefact(random_pokemon)
+    #data = call_api()
+
+    #Generate the message and publish to sns topic
+    response = client.publish(
+    TopicArn='arn:aws:sns:us-east-1:205758311321:pokemon-fun-facts-topic',
+    Message='Random Pokemon of the day is:\n'
+            'name:'+data[1]+'\n'
+            'id:'+data[0]+'\n'
+            'height:'+data[2]+'\n'
+            'weight:'+data[3]+'\n' ,
+    Subject='Random Poke Facts',
+    MessageStructure='string',
+    MessageAttributes={
+        'string': {
+            'DataType': 'string',
+            'StringValue': 'string',
+            'BinaryValue': b'bytes'
+        }
+    },
+)
     return  iterate_json(data)
 
 
 
 ####################### MAIN #########################
 
-random_pokemon = str(randrange(100000))
-#print(random_pokemon)
+random_pokemon = str(randrange(1,95)) # generate random pokemon 
+print(random_pokemon)
 data = call_api_random_pokefact(random_pokemon)
 
 # data = call_api()
