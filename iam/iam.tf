aa4f826fecd9696b4d5fd8a1c9a665a72bf5307a
Rafael Carmilema <rafaelcarmilema@gmail.com>
2022-08-18 21:45:34 -0400

    fixed

--- iam/iam.tf
+++ iam/iam.tf
@@ -17,7 +17,28 @@ resource "aws_iam_role" "iam_for_lambda" {
 }
 EOF
 }
+resource "aws_iam_role_policy" "test_policy" {
+  name = "test_policy"
+  role = aws_iam_role.iam_for_lambda.id
 
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = <<EOF
+    {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+        "Sid": "Stmt1660873417093",
+        "Action": [
+            "ssm:PutParameter"
+        ],
+        "Effect": "Allow",
+        "Resource": "arn:aws:lambda:us-east-1:205758311321:function:lambda_function_pokemon"
+        }
+      ]
+    }
+  EOF
+}
 output "lambda_role" {
   value = aws_iam_role.iam_for_lambda.arn
 }
